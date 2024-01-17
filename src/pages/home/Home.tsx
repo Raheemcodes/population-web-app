f96828f200ea125a353f05e6ffd1ec1341d30942
Raheemcodes <raheemscorporatemail@gmail.com>
2024-01-17 22:46:55 +0100

    Create and Add Search Input

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,10 @@
+import SearchInput from '../../components/search/Search';
 import classes from './Home.module.scss';
 
 const HomePage = () => {
   return (
     <main className={classes.main}>
-      <h1>Home Page</h1>
+      <SearchInput />
     </main>
   );
 };
