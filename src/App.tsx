1e5e88d6ba1a95e0fb86c55ef78c4582729d70e3
Raheemcodes <raheemscorporatemail@gmail.com>
2024-01-17 22:06:58 +0100

    Implement Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.scss';
+import RootLayout from './pages/Root';
+import HomePage from './pages/home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '',
+    element: <RootLayout />,
+    children: [{ index: true, element: <HomePage /> }],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/country/Country.tsx
@@ -0,0 +1,5 @@
+const CountryPage = () => {
+  return <h1>Country Page</h1>;
+};
+
+export default CountryPage;
